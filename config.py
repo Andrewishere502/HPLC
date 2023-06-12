54a2a38e3bad548c14ede68777464914551d27df
Andrewishere502 <35299674+Andrewishere502@users.noreply.github.com>
2023-06-12 09:54:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
-#Carndenolide File Name
+# Absolute or relative path to the txt file containing cardenolide peak
+# retention times.
 CARD_TXT = "card.txt"
 
-#Phenylpropanoid File Name
+# Absolute or relative path to the txt file containing phenylpropanoid
+# peak retention times.
 PP_TXT = "pp.txt"
 
-#Margin for Cardenolide buckets
+# Margin for Cardenolide buckets
 CARD_MARGIN = 0.2
 
-#Margin for Phenylpropanoid buckets
+# Margin for Phenylpropanoid buckets
 PP_MARGIN = 0.25
 
-PROCESSED_INPUT_FOLDER = "ProcessedInput"
-
+# The absolute or relative path to the directory where raw input is
+# stored. This must exist before running accumulate_report01s.py.
+# Do not include a '/' at the end.
 RAW_INPUT_FOLDER = "RawInput"
 
+# The absolute or relative path to the directory where processed input
+# will be stored. If it doesn't exist, it will be created for you.
+# Do not include a '/' at the end.
+PROCESSED_INPUT_FOLDER = "ProcessedInput"
+
+# The absolute or relative path to the directory where the final output
+# will be stored. If it doesn't exist, it will be created for you.
+# Do not include a '/' at the end.
 OUTPUT_FOLDER = "Output"
