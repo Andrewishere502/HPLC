5dba91ad072e7a434f61ab65bdf0c86ee99adfbe
Andrewishere502 <35299674+Andrewishere502@users.noreply.github.com>
2023-06-12 10:12:11 -0500

    Update accumulate_report01s.py

--- accumulate_report01s.py
+++ accumulate_report01s.py
@@ -1,136 +1,87 @@
-"""Generate a final dataset from the accumulated Report01.csv files.
-Run after accumulate_report01s.py.
+"""Take the peak number, retention time, and area from each of the
+Report01.csv files scattered throughout an HPLC Chromatogram directory,
+and put that data in a file named after the directory the data was
+found in.
 """
 # Standard library imports
 import os
 
 # Third party imports
 import pandas as pd
-import numpy as np
 
 # Import from our app
 from logger import Logger
 import config
 
 
-def get_chem_id(ret_time, chemmeta_df):
-    """Return the chem_id associated with the retention range ret_time
-    falls in.
-    """
-    for line in chemmeta_df.values:
-        begin_ret_time, end_ret_time, chem_id = list(line)
-        if ret_time >= float(begin_ret_time) and ret_time <= float(end_ret_time):
-            return chem_id
-    return  # no id found, return None
+# Create an object to log errors that occur
+logger = Logger("", "log.txt", erase_on_init=True)
 
+# RawInput is the folder to put all of the raw data into. RIF stands
+# for raw input folder
+# RIF = "RawInput"
+RIF = config.RAW_INPUT_FOLDER
+if not os.path.exists(RIF):
+    raise OSError("RawInput folder not found. Please ensure it exists in your path and is named correctly.")
 
-def get_meta_data(parent_directory, filename, meta_df):
-    """Return the meta data for a file name found in a specific
-    directory.
-    """
-    # Narrow down the meta_df by the parent folder name
-    dir_df = meta_df[meta_df["ParentFolderName"] == parent_directory]
-    # Now filter out all rows that do not match the file name
-    file_df = dir_df[dir_df["FileName"] == filename]
-
-    # Convert those values into a list. If there were no matching rows
-    # found, catch the IndexError and return an list full of None.
-    try:
-        meta_data = list(file_df.values[0])
-    except IndexError:
-        meta_data = ["NA"] * len(meta_df.columns)
-    return dict(zip(meta_df.columns, meta_data))
-
-
-# Create an object to log errors that occur. Don't erase the contents
-# when run to preserve error logs from accumulate_report01s.py
-logger = Logger("", "log.txt", erase_on_init=False)
-
-# Define a folder for where all intermediate input data should be.
-# PIF stands for processed input folder.
+# Define a folder for where all processed data should go. PIF stands
+# for processed input folder.
 PIF = config.PROCESSED_INPUT_FOLDER
-# Raise an error if the ProcessedInput folder doesn't exist because
-# that means the user hasn't run accumulate_report01s.py yet.
 if not os.path.exists(PIF):
-    raise OSError("The directory ProcessedInput does not exist. Please run accumulate_report01s.py to create and populate it.")
+    print(f"{config.PROCESSED_INPUT_FOLDER} directory did not exist: Created {config.PROCESSED_INPUT_FOLDER}.")
+    os.mkdir(config.PROCESSED_INPUT_FOLDER)
 
-# Define a folder where the output will go. OF stands for output folder
-OF = config.OUTPUT_FOLDER
-# Make the output folder if it doesn't exist
-if not os.path.exists(OF):
-    print("Output directory did not exist: Created Output.")
-    os.mkdir("Output")
+# Get a list of all the raw data folders. Parse non-folders by excluding
+# anything with a file extension.
+raw_data_folders = [folder for folder in os.listdir(RIF)
+                    if len(folder.split(".")) == 1]
+folder_counter = 1
+for folder_name in raw_data_folders:
+    print(f"Starting folder {folder_counter}/{len(raw_data_folders)} {folder_name}")
+    # Get all of the folders which have relevant Report01.csv files,
+    # sorted alphanumerically.
+    filenames = sorted([fn for fn in os.listdir(f"{RIF}/{folder_name}") if fn[-2:] == ".D"])
 
-# Get the chemical meta data for reference later
-chemmeta_df = pd.read_csv("chemmeta.csv")
-# Get a list of all unique chemical ids
-all_chem_ids = list(set(chemmeta_df["ChemicalID"]))
+    # Make a DataFrame to store all of the Report01 files found within
+    # a directory's sub-directories.
+    folder_df = pd.DataFrame(columns=["FileName", "PeakNum", "RetTime", "Area"])
 
-# Get the sample meta data for reference later
-meta_df = pd.read_csv("supermeta.csv")
-# Sort the meta data for easier reference
-meta_df = meta_df.sort_values(by=["FileName"])
-# Get a list of the columns in meta_df
-meta_columns = list(meta_df.columns)
+    print(f"\t~Processing raw data files ({len(filenames)}):")
+    file_counter = 1
+    for filename in filenames:
+        print(f"\t\t-> {file_counter}/{len(filenames)} {filename}")
+        # Catch error thrown if no Report01.csv file is found
+        try:
+            # Get the contents of the old file
+            with open(f"{RIF}/{folder_name}/{filename}/Report01.csv", "r", encoding="utf-16") as file:
+                # Read lines then close to save memory
+                lines = file.readlines()
 
-# DataFrame where all the output will be located
-output_df = pd.DataFrame(columns=(meta_columns + sorted(all_chem_ids)))
+            # Store the file name, peak number, retention time, and
+            # area in each line of the .csv file as a row in the
+            # dataframe.
+            for line in lines:
+                # Get relevant data from the line, exclude
+                # irrelevant data with _
+                peak_num, ret_time, _, _, area, *_ = line.split(",")
+                # Add as the last row in folder_df
+                folder_df.loc[len(folder_df.index)] = (filename, peak_num, ret_time, area)
+            # Increment file counter
+            file_counter += 1
+                
+        except FileNotFoundError:
+            # This is a non-fatal error, log it and skip this file
+            logger.log(f"There was no Report01.csv file for {RIF}/{folder_name}/{filename}.")
+            # Increment file counter
+            file_counter += 1
+            continue  # skip to next in filenames
 
-# Get all csv files by name into a list.
-processed_filenames = [fn for fn in os.listdir(PIF) if fn[-4:] == ".csv"]
-processed_file_counter = 1
-for processed_filename in processed_filenames:
-    print(f"Starting processed file {processed_file_counter}/{len(processed_filenames)} {processed_filename}")
-    # Read the processed csv file into a DataFrame
-    df = pd.read_csv(f"{PIF}/{processed_filename}")
+    # Save the folder's DataFrame if it is not empty.
+    if len(folder_df.values) > 0:
+        folder_df.to_csv(f"{PIF}/acc_{folder_name}.csv", index=False, index_label=False)
+        print("\t~Processed and saved")
+    else:
+        print("\t~Folder was empty")
     
-    # Remove the acc_ and .csv parts from the processed_filename
-    # to get the parent directory name of the files referenced within
-    # the csv.
-    parent_directory = processed_filename[4:].replace(".csv", "")
-    
-    # Get a list of the file names listed inside the processed file,
-    # and add one row detailing the chemicals found in that sample for
-    # each file name.
-    unique_inner_filenames = set([*df["FileName"].values])
-    print(f"\t~Finalizing data ({len(unique_inner_filenames)}):")
-    unique_inner_counter = 1
-    for filename in unique_inner_filenames:
-        print(f"\t\t-> {unique_inner_counter}/{len(unique_inner_filenames)} {filename}")
-        # Get the meta data for this file
-        meta_data = get_meta_data(parent_directory, filename, meta_df)
-
-        # Assemble the output row for output_df
-        output_row = meta_data
-        output_row.update({chem_id: None for chem_id in sorted(all_chem_ids)})
-
-        # Get a DataFrame for all the retention times and areas for
-        # this file name
-        chem_data = df[df["FileName"] == filename][["RetTime", "Area"]].values
-        for ret_time, area in chem_data:
-            chem_id = get_chem_id(ret_time, chemmeta_df)
-            if chem_id != None:
-                output_row.update({chem_id: area})
-        
-        # Update the output DataFrame
-        output_df = output_df.append(output_row, ignore_index=True)
-        
-        # Update unique inner file counter
-        unique_inner_counter += 1
-
-    # Update processed file progression counter
-    processed_file_counter += 1
-
-# Replace all na chem ids with 1
-for chem_id in all_chem_ids:
-    output_df[chem_id].replace(np.NaN, 1, inplace=True)
-
-# Replace any nan in ProportionSyriaca with "na"
-output_df["ProportionSyriaca"].replace(np.NaN, "na", inplace=True)
-
-# Save the final dataset if it is not empty
-if len(output_df.values) > 0:
-    output_df.to_excel(f"{OF}/finalDataset.xlsx")
-    print(f"*Finalized and saved*")
-else:
-    print(f"*No data to save*")
+    # Update the folder counter
+    folder_counter += 1
