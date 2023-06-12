193e0b9390a2642783512a6a22271b0cbe0c7ed9
ScottShlanta <99041082+ScottShlanta@users.noreply.github.com>
2023-06-12 09:25:02 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+#Carndenolide File Name
+CARD_TXT = "card.txt"
+
+#Phenylpropanoid File Name
+PP_TXT = "pp.txt"
+
+#Margin for Cardenolide buckets
+CARD_MARG = 0.2
+
+#Margin for Phenylpropanoid buckets
+PP_MARG = 0.25
+
+PROCESSED_INPUT_FOLDER = "ProcessedInput"
+
+RAW_INPUT_FOLDER = "RawInput"
+
+OUTPUT_FOLDER = "Output"
