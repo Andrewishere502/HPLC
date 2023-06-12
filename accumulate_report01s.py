4950ed7f985ab58a3df49c8a7ac15a7ff8bc984b
Andrewishere502 <35299674+Andrewishere502@users.noreply.github.com>
2023-06-12 09:22:22 -0500

    Update accumulate_report01s.py

--- accumulate_report01s.py
+++ accumulate_report01s.py
@@ -24,15 +24,16 @@ if not os.path.exists(RIF):
 # for processed input folder.
 PIF = "ProcessedInput"
 if not os.path.exists(PIF):
-    print("ProcessedInput directory did not exist: Created ProcessedInput.")
-    os.mkdir("ProcessedInput")
+    print(f"ProcessedInput directory did not exist: Created {PIF}.")
+    os.mkdir(PIF)
 
 # Get a list of all the raw data folders. Parse non-folders by excluding
 # anything with a file extension.
 raw_data_folders = [folder for folder in os.listdir(RIF)
                     if len(folder.split(".")) == 1]
-
+folder_counter = 1
 for folder_name in raw_data_folders:
+    print(f"Starting folder {folder_counter}/{len(raw_data_folders)} {folder_name}")
     # Get all of the folders which have relevant Report01.csv files,
     # sorted alphanumerically.
     filenames = sorted([fn for fn in os.listdir(f"{RIF}/{folder_name}") if fn[-2:] == ".D"])
@@ -41,26 +42,42 @@ for folder_name in raw_data_folders:
     # a directory's sub-directories.
     folder_df = pd.DataFrame(columns=["FileName", "PeakNum", "RetTime", "Area"])
 
+    print(f"\t~Processing raw data files ({len(filenames)}):")
+    file_counter = 1
     for filename in filenames:
+        print(f"\t\t-> {file_counter}/{len(filenames)} {filename}")
         # Catch error thrown if no Report01.csv file is found
         try:
             # Get the contents of the old file
             with open(f"{RIF}/{folder_name}/{filename}/Report01.csv", "r", encoding="utf-16") as file:
-                # Store the file name, peak number, retention time, and
-                # area in each line of the .csv file as a row in the
-                # dataframe.
-                for line in file.readlines():
-                    # Get relevant data from the line, exclude
-                    # irrelevant data with _
-                    peak_num, ret_time, _, _, area, *_ = line.split(",")
-                    # Add as the last row in folder_df
-                    folder_df.loc[len(folder_df.index)] = (filename, peak_num, ret_time, area)
+                # Read lines then close to save memory
+                lines = file.readlines()
+
+            # Store the file name, peak number, retention time, and
+            # area in each line of the .csv file as a row in the
+            # dataframe.
+            for line in lines:
+                # Get relevant data from the line, exclude
+                # irrelevant data with _
+                peak_num, ret_time, _, _, area, *_ = line.split(",")
+                # Add as the last row in folder_df
+                folder_df.loc[len(folder_df.index)] = (filename, peak_num, ret_time, area)
+            # Increment file counter
+            file_counter += 1
                 
         except FileNotFoundError:
             # This is a non-fatal error, log it and skip this file
             logger.log(f"There was no Report01.csv file for {RIF}/{folder_name}/{filename}.")
+            # Increment file counter
+            file_counter += 1
             continue  # skip to next in filenames
 
     # Save the folder's DataFrame if it is not empty.
     if len(folder_df.values) > 0:
         folder_df.to_csv(f"{PIF}/acc_{folder_name}.csv", index=False, index_label=False)
+        print("\t~Processed and Saved")
+    else:
+        print("\t~Folder was empty")
+    
+    # Update the folder counter
+    folder_counter += 1
