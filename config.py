a9d7d6242958bbe5236415e2d299e3f96e9aced4
ScottShlanta <99041082+ScottShlanta@users.noreply.github.com>
2023-06-16 15:22:37 -0500

    Update config.py
    Update from MOIST

--- config.py
+++ config.py
@@ -1,38 +1,42 @@
 # Absolute or relative path to the txt file containing cardenolide peak
 # retention times.
-CARD_TXT = "/Volumes/OlsonLab/card.txt"
+CARD_TXT = "card.txt"
 
 # Absolute or relative path to the txt file containing phenylpropanoid
 # peak retention times.
-PP_TXT = "/Volumes/OlsonLab/pp.txt"
+PP_TXT = "pp.txt"
 
-# Margin for Cardenolide buckets
-CARD_MARGIN = 0.20
+# Margin for buckets
+BUCKET_MARGIN = 0.1
 
-# Margin for Phenylpropanoid buckets
-PP_MARGIN = 0.25
+# Whether or not chemmeta should delete ambiguous labels
+DELETE_AMBIG = True
+
+# Max and minimum range for a compound to be recognized
+MAX_RANGE = 1.3
+MIN_RANGE = 0.2
 
 # The absolute or relative path to the .csv file that stores the
 # chemical meta data. This must exist before running
 # generate_dataset.py. Must be a csv file.
-CHEM_META_FILE = "chemmeta.csv"
+CHEM_META_FILE = f"HPLC-main\\chemmeta_{BUCKET_MARGIN}_Ambig{not DELETE_AMBIG}.csv"
 
 # The absolute or relative path to the .csv file that stores the
 # sample (super) meta data. This must exist before running
 # generate_dataset.py. Must be a csv file.
-SUPER_META_FILE = "/Volumes/OlsonLab/supermeta.csv"
+SUPER_META_FILE = "supermeta.csv"
 
 # The absolute or relative path to the directory where raw input is
 # stored. This must exist before running accumulate_report01s.py.
 # Do not include a '/' at the end.
-RAW_INPUT_FOLDER = "/Volumes/OlsonLab/RawInput"
+RAW_INPUT_FOLDER = "RawInput"
 
 # The absolute or relative path to the directory where processed input
 # will be stored. If it doesn't exist, it will be created for you.
 # Do not include a '/' at the end.
-PROCESSED_INPUT_FOLDER = "/Volumes/OlsonLab/ProcessedInput"
+PROCESSED_INPUT_FOLDER = "ProcessedInput"
 
 # The absolute or relative path to the directory where the final output
 # will be stored. If it doesn't exist, it will be created for you.
 # Do not include a '/' at the end.
-OUTPUT_FOLDER = "/Volumes/OlsonLab/Output"
+OUTPUT_FOLDER = "Output"
