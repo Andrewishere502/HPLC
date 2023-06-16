a511e14b0856c7a2037bf73383a5b1100c0a3c1a
ScottShlanta <99041082+ScottShlanta@users.noreply.github.com>
2023-06-16 15:21:08 -0500

    Update generate_chemmeta.py
    Update from MOIST

--- generate_chemmeta.py
+++ generate_chemmeta.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import re
 
 import pandas as pd
 
@@ -84,7 +85,16 @@ def group_peaks(peaks, margin):
             buckets[-1].add_peak(peak)
         else:
             buckets.append(Bucket([peak]))
-    return buckets
+
+    # Remove any buckets outside of an allowed range
+    valid_buckets = []
+    for bucket in buckets:
+        # Check if the bucket's range is between the
+        # required bounds.
+        if (bucket.max - bucket.min > config.MIN_RANGE
+            and bucket.max - bucket.min < config.MAX_RANGE):
+            valid_buckets.append(bucket)
+    return valid_buckets
 
 def name_chem_range(id_prefix, mean):
     """Return the name for a chemical range's mean."""
@@ -121,16 +131,16 @@ with open(config.CHEM_META_FILE, "w") as chemmeta_file:
     chemmeta_file.write("BeginRetTime,EndRetTime,ChemicalID\n")
 
     # Get the cardenolide specific buckets
-    card_buckets = group_peaks(load_peaks_txt(config.CARD_TXT), config.CARD_MARGIN)
+    card_buckets = group_peaks(load_peaks_txt(config.CARD_TXT), config.BUCKET_MARGIN)
     # Write the card buckets to chemmeta
     write_buckets(card_buckets, chemmeta_file, id_prefix="C")
-    print(f"Number of cardenolide buckets: {len(card_buckets)}")
+    print(f"Number of initial cardenolide buckets: {len(card_buckets)}")
 
     # Get the phenylpropanoid specific buckets
-    pp_buckets =  group_peaks(load_peaks_txt(config.PP_TXT), config.PP_MARGIN)
-    # Write teh card buckets to chemmeta
+    pp_buckets =  group_peaks(load_peaks_txt(config.PP_TXT), config.BUCKET_MARGIN)
+    # Write the card buckets to chemmeta
     write_buckets(pp_buckets, chemmeta_file, id_prefix="PP")
-    print(f"Number of phenylpropanoid buckets: {len(pp_buckets)}")
+    print(f"Number of initial phenylpropanoid buckets: {len(pp_buckets)}")
 
 
 # Get all unique overlapping card and pp pairs
@@ -157,103 +167,140 @@ for line in lines:
             hits.add(tuple(sorted((chem_id[:-1], chem_id2[:-1]))))
             #hits.add((chem_id, chem_id2))
 
-
-# Fix all overlaps
+# Handle overlapping PPs and Cs
 delete_me = set()
 df = pd.read_csv(config.CHEM_META_FILE, index_col=2)
 for c_name, pp_name in hits:
-    c1, c2 = df.loc[c_name]
-    p1, p2 = df.loc[pp_name]
+    # Delete all of the ambiguous Cs and PPs
+    if config.DELETE_AMBIG:
+        # Get the range of the cardenolide
+        c_start, c_end = list(df.loc[c_name])
+        c_range = c_end - c_start
+        # Get the range of the phenylpropanoid
+        pp_start, pp_end = list(df.loc[pp_name])
+        pp_range = pp_end - pp_start
+        # Delete the compound with the smaller range,
+        # if they have the same range then delete both
+        if c_range < pp_range:
+            delete_me.add(c_name)
+        elif pp_range < c_range:
+            delete_me.add(pp_name)
+        else:
+            delete_me.add(c_name)
+            delete_me.add(pp_name)
 
-    # Start-End overlap
-    if c1 < p1 and c2 < p2:
-        # Modify C and PP
-        change_chem_times(df, c_name, pp_name, c1, p1, c2, p2)
-        # Add a CPP
-        merge_chem_times(df, p1 + 0.1, c2 - 0.1)
-    elif p1 < c1 and p2 < c2:
-        # Modify C and PP
-        change_chem_times(df, c_name, pp_name, p2, c2, p1, c1)
-        # Add a CPP
-        merge_chem_times(df, c1 + 0.1, p2 - 0.1)
-
-    # Contains
-    elif c1 < p1 and c2 > p2:
-        # Add two Cs
-        c_name1 = name_chem_range("C", (c1 + p1) / 2)
-        c_name2 = name_chem_range("C", (p2 + c2) / 2)
-        change_chem_times(df, c_name1, c_name2, c1, p1, p2, c2)
-        # Remove old C
-        delete_me.add(c_name)
-        # Add a CPP
-        merge_chem_times(df, p1 + 0.1, p2 - 0.1)
-    elif c1 > p1 and c2 < p2:
-        # Add two PPs
-        p_name1 = name_chem_range("PP", (p1 + c1) / 2)
-        p_name2 = name_chem_range("PP", (c2 + p2) / 2)
-        change_chem_times(df, p_name1, p_name2, p1, c1, c2, p2)
-        # Remove old PP
-        delete_me.add(pp_name)
-        # Add a CPP
-        merge_chem_times(df, c1 + 0.1, c2 - 0.1)
-
-    # Start-End same
-    elif c2 == p1 and c1 < p2:
-        # Just shift the p range over by 0.1
-        change_chem_times(df, c_name, pp_name, c1, c2, p1 + 0.1, p2)
-    elif p2 == c1 and p1 < c2:
-        # Just shift the c range over by 0.1
-        change_chem_times(df, c_name, pp_name, c1 + 0.1, c2, p1, p2)
-    
-    # Start-Start same
-    elif c1 == p1 and c2 < p2:
-        # Add a new PP range
-        add_chem(df, name_chem_range("PP", (c2 + p2) / 2), c2, p2)
-        # Delete old C range
-        delete_me.add(c_name)
-        # Add a CPP
-        merge_chem_times(df, c1, c2 - 0.1)
-    elif c1 == p1 and c2 > p2:
-        # Add a new C range
-        add_chem(df, name_chem_range("C", (p2 + c2) / 2), p2, c2)
-        # Delete old PP range
-        delete_me.add(pp_name)
-        # Add a CPP
-        merge_chem_times(df, p1, p2 - 0.1)
-
-    # End-End same
-    elif c2 == p2 and c1 < p1:
-        # Add a new C range
-        add_chem(df, name_chem_range("C", (c1 + p1) / 2), c1, p1)
-        # Delete old PP range
-        delete_me.add(pp_name)
-        # Add cpp
-        merge_chem_times(df, p1 + 0.1, p2)
-
-    elif c2 == p2 and c1 > p1:
-        # Add a new C range
-        add_chem(df, name_chem_range("PP", (p1 + c1) / 2), p1, c1)
-        # Delete old PP range
-        delete_me.add(c_name)
-        # Add cpp
-        merge_chem_times(df, c1 + 0.1, c2)
-
-    # Start-Start and End-End same
-    elif c1 == p1 and c2 == p2:
-        # Remove the old C range
-        delete_me.add(c_name)
-        # Remove the old PP range
-        delete_me.add(pp_name)
-        # Merge
-        merge_chem_times(df, c1, p1)
-
-    # Safety catch-all
+    # Label ambiguous Cs and PPs
     else:
-        raise ValueError(f"Oh crap... {c1} {c2} and {p1} {p2} should overlap but didn't?")
 
+        c1, c2 = df.loc[c_name]
+        p1, p2 = df.loc[pp_name]
+
+        # Start-End overlap
+        if c1 < p1 and c2 < p2:
+            # Modify C and PP
+            change_chem_times(df, c_name, pp_name, c1, p1, c2, p2)
+            # Add a CPP
+            merge_chem_times(df, p1 + 0.1, c2 - 0.1)
+        elif p1 < c1 and p2 < c2:
+            # Modify C and PP
+            change_chem_times(df, c_name, pp_name, p2, c2, p1, c1)
+            # Add a CPP
+            merge_chem_times(df, c1 + 0.1, p2 - 0.1)
+
+        # Contains
+        elif c1 < p1 and c2 > p2:
+            # Add two Cs
+            c_name1 = name_chem_range("C", (c1 + p1) / 2)
+            c_name2 = name_chem_range("C", (p2 + c2) / 2)
+            change_chem_times(df, c_name1, c_name2, c1, p1, p2, c2)
+            # Remove old C
+            delete_me.add(c_name)
+            # Add a CPP
+            merge_chem_times(df, p1 + 0.1, p2 - 0.1)
+        elif c1 > p1 and c2 < p2:
+            # Add two PPs
+            p_name1 = name_chem_range("PP", (p1 + c1) / 2)
+            p_name2 = name_chem_range("PP", (c2 + p2) / 2)
+            change_chem_times(df, p_name1, p_name2, p1, c1, c2, p2)
+            # Remove old PP
+            delete_me.add(pp_name)
+            # Add a CPP
+            merge_chem_times(df, c1 + 0.1, c2 - 0.1)
+
+        # Start-End same
+        elif c2 == p1 and c1 < p2:
+            # Just shift the p range over by 0.1
+            change_chem_times(df, c_name, pp_name, c1, c2, p1 + 0.1, p2)
+        elif p2 == c1 and p1 < c2:
+            # Just shift the c range over by 0.1
+            change_chem_times(df, c_name, pp_name, c1 + 0.1, c2, p1, p2)
+    
+        # Start-Start same
+        elif c1 == p1 and c2 < p2:
+            # Add a new PP range
+            add_chem(df, name_chem_range("PP", (c2 + p2) / 2), c2, p2)
+            # Delete old C range
+            delete_me.add(c_name)
+            # Add a CPP
+            merge_chem_times(df, c1, c2 - 0.1)
+        elif c1 == p1 and c2 > p2:
+            # Add a new C range
+            add_chem(df, name_chem_range("C", (p2 + c2) / 2), p2, c2)
+            # Delete old PP range
+            delete_me.add(pp_name)
+            # Add a CPP
+            merge_chem_times(df, p1, p2 - 0.1)
+
+        # End-End same
+        elif c2 == p2 and c1 < p1:
+            # Add a new C range
+            add_chem(df, name_chem_range("C", (c1 + p1) / 2), c1, p1)
+            # Delete old PP range
+            delete_me.add(pp_name)
+            # Add cpp
+            merge_chem_times(df, p1 + 0.1, p2)
+
+        elif c2 == p2 and c1 > p1:
+            # Add a new C range
+            add_chem(df, name_chem_range("PP", (p1 + c1) / 2), p1, c1)
+            # Delete old PP range
+            delete_me.add(c_name)
+            # Add cpp
+            merge_chem_times(df, c1 + 0.1, c2)
+
+        # Start-Start and End-End same
+        elif c1 == p1 and c2 == p2:
+            # Remove the old C range
+            delete_me.add(c_name)
+            # Remove the old PP range
+            delete_me.add(pp_name)
+            # Merge
+            merge_chem_times(df, c1, p1)
+
+        # Safety catch-all
+        else:
+            raise ValueError(f"Oh crap... {c1} {c2} and {p1} {p2} should overlap but didn't?")
 
 # Drop all necessary rows
 df.drop(labels=list(delete_me), axis=0, inplace=True)
+
+# Count PP, C, and CPP labels
+PP_count = 0
+C_count = 0
+CPP_count = 0
+for chem in df.index:
+    if re.match(r"PP\d+\.\d_Area", chem):
+        #print(chem, "is a PP")
+        PP_count += 1
+    elif re.match(r"CPP\d+\.\d_Area", chem):
+        #print(chem, "is a CPP")
+        CPP_count += 1
+    elif re.match(r"C\d+\.\d_Area", chem):
+        #print(chem, "is a C")
+        C_count += 1
+print(f"Number of initial cardenolide buckets: {C_count}")
+print(f"Number of initial phenylpropanoid buckets: {PP_count}")
+print(f"Number of initial ambiguous buckets: {CPP_count}")
 
 # Save the csv
 df.to_csv(config.CHEM_META_FILE)
